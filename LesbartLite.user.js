18a17d7c81fddab2154c8c86692ecd4e3e810547
AnBasement <159536871+AnBasement@users.noreply.github.com>
2025-07-28 22:54:47 +0200

    Create LesbartLite.user.js

---
+++ LesbartLite.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Gjør DB.no mer lesbar
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Gjør Dagbladets nettsider mer lesbare.
+// @author       AnBasement
+// @match        https://www.dagbladet.no/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Fjerner elementer med rullende tekst
+    function fjernRullendeTekst() {
+        document.querySelectorAll('div.breaking-rolling-text-slug').forEach(el => el.remove());
+    }
+
+    // Kjør når siden lastes inn
+    removeBreakingRollingText();
+
+    // Kjør ved dynamisk innlasting
+    const observer = new MutationObserver(() => {
+        fjernRullendeTekst();
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
